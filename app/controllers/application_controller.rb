f560c4875b2f857ca104cd06b59f645b3b82066e
nonogogono <morinaga_dars_stream@yahoo.co.jp>
2020-02-02 10:49:10 +0000

    delete Japanese

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "My First Step\n冒険の一歩を踏み出した！"
+    render html: "My First Step"
   end
 end
