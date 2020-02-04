db07d3a9562e6387a8cc5579d80792a5cf5ba658
nonogogono <morinaga_dars_stream@yahoo.co.jp>
2020-02-04 22:37:06 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,37 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title  = "どんなサイトにしよっかな"
+  end
+    
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}" 
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+  
+    test "should get profile" do
+    get static_pages_profile_url
+    assert_response :success
+    assert_select "title", "Profile | #{@base_title}"
+  end
 end
